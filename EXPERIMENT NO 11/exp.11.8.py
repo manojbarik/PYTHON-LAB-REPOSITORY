08565fbfa5f689eca766e718b1215df0a0a72217
manojbarik <MANOJBARIK84998@GMAIL.COM>
2026-03-27 16:04:33 +0530

    Add files via upload

---
+++ EXPERIMENT NO 11/exp.11.8.py
@@ -0,0 +1,10 @@
+#  WAP to to read CSV and JSON files.
+# Reading a CSV file
+import pandas as pd 
+df_csv = pd.read_csv('sample.csv') # Replace 'sample.csv' with your file path
+print("DataFrame from CSV:")
+print(df_csv)
+# Reading a JSON file
+df_json = pd.read_json('sample.json') # Replace 'sample.json' with your file path
+print("\nDataFrame from JSON:")
+print(df_json)
